18f2232902760c9a2e8d9fd53082db025af6fa1b
EVogel1999 <eav990@gmail.com>
2019-03-04 20:14:38 -0500

    Generate TaskListComponent

--- quick-quill/src/app/app.module.ts
+++ quick-quill/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
