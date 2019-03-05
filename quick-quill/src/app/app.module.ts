17c706a6919d8defb7926e7766c19e28b7c20197
EVogel1999 <eav990@gmail.com>
2019-03-04 20:23:13 -0500

    Generate task list item component

--- quick-quill/src/app/app.module.ts
+++ quick-quill/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskListItemComponent
   ],
   imports: [
     BrowserModule,
