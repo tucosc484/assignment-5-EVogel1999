091a45a705c9ad51e8e45f0ad96b976ab01da810
EVogel1999 <eav990@gmail.com>
2019-03-04 20:25:41 -0500

    Set up logic for task list view component

--- quick-quill/src/app/task-list-item/task-list-item.component.ts
+++ quick-quill/src/app/task-list-item/task-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-list-item',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list-item.component.scss']
 })
 export class TaskListItemComponent implements OnInit {
+
+  @Input()
+  task;
 
   constructor() { }
 
--- quick-quill/src/app/task-list/task-list.component.ts
+++ quick-quill/src/app/task-list/task-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListComponent implements OnInit {
 
+  tasks;
+
   constructor() { }
 
   ngOnInit() {
