56eeddc6c6ae2e770386d2565da04b661c59f3d8
EVogel1999 <eav990@gmail.com>
2019-03-04 19:49:48 -0500

    Set up angular routes

--- quick-quill/src/app/app-routing.module.ts
+++ quick-quill/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'tasks', component: TaskListComponent },
+  // { path: 'tasks/create', component: CreateTaskComponent },
+  // { path: 'tasks/:id', component: TaskDetailsComponent },
+  // { path: '', redirectTo: '/tasks', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
