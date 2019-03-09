99a9b4c9bdf4c82e086af0d2e020a5a2e6d54a68
EVogel1999 <eav990@gmail.com>
2019-03-09 18:58:16 -0500

    Create routes for creating and viewing a task

--- quick-quill/src/app/app-routing.module.ts
+++ quick-quill/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TaskListComponent } from './task-list/task-list.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 const routes: Routes = [
   { path: 'tasks', component: TaskListComponent },
-  // { path: 'tasks/create', component: CreateTaskComponent },
-  // { path: 'tasks/:id', component: TaskDetailsComponent },
+  { path: 'tasks/create', component: CreateTaskComponent },
+  { path: 'tasks/:id', component: TaskDetailsComponent },
   { path: '', redirectTo: '/tasks', pathMatch: 'full' }
 ];
 
