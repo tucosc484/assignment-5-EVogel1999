bd891f1692831fbc6b4d00af99c3a192bb56cd72
EVogel1999 <eav990@gmail.com>
2019-03-12 13:47:25 -0400

    Comment app component

--- quick-quill/src/app/app.component.ts
+++ quick-quill/src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+/**
+ * Empty App Component class.
+ * Wraps the navbar and the router outlet in the template.
+ */
 export class AppComponent {}
